37406977c3374370326cc446ab2931eb83902b21
Kyle J. Harms <kyle.harms@cornell.edu>
2024-11-13 12:18:56 -0500

    lab 12 solution

--- pages/grade-edit.php
+++ pages/grade-edit.php
@@ -18,31 +18,33 @@ $page_title = "Transcript";
 
 $nav_transcript_class = "active_page";
 
-include_once("includes/transcript-values.php");
+if (is_user_logged_in()) {
+  include_once("includes/transcript-values.php");
 
-if (isset($_POST["request-insert"])) {
-  $record_id = ($_POST["grade_id"] == "" ? NULL : (int)$_POST["grade_id"]);
-} else {
-  // get the record id for the grade
-  $record_id = ($_GET["record"] == "" ? NULL : (int)$_GET["record"]);
-}
-
-// Get the record using the `id` from the DB.
-if ($record_id) {
-  $records = exec_sql_query(
-    $db,
-    "SELECT * FROM grades WHERE (id = :id);",
-    array(
-      ":id" => $record_id
-    )
-  )->fetchAll();
-
-  // Did we find the record?
-  if (count($records) > 0) {
-    $record = $records[0]; // first record
+  if (isset($_POST["request-insert"])) {
+    $record_id = ($_POST["grade_id"] == "" ? NULL : (int)$_POST["grade_id"]);
+  } else {
+    // get the record id for the grade
+    $record_id = ($_GET["record"] == "" ? NULL : (int)$_GET["record"]);
   }
-} else {
-  $record = NULL;
+
+  // Get the record using the `id` from the DB.
+  if ($record_id) {
+    $records = exec_sql_query(
+      $db,
+      "SELECT * FROM grades WHERE (id = :id);",
+      array(
+        ":id" => $record_id
+      )
+    )->fetchAll();
+
+    // Did we find the record?
+    if (count($records) > 0) {
+      $record = $records[0]; // first record
+    }
+  } else {
+    $record = NULL;
+  }
 }
 ?>
 <!DOCTYPE html>
@@ -57,88 +59,98 @@ if ($record_id) {
 
     <h2><?php echo $page_title; ?></h2>
 
-    <?php if ($record == NULL): ?>
+    <?php if (is_user_logged_in()): ?>
 
-      <p>Unknown grade record (<?php echo htmlspecialchars($record_id); ?>).</p>
-      <p>Please contact your site administrator for assistance.</p>
-      <p>Return to the <a href="/transcript">transcript</a>.
+      <?php if ($record == NULL): ?>
 
-      <?php else: ?>
+        <p>Unknown grade record (<?php echo htmlspecialchars($record_id); ?>).</p>
+        <p>Please contact your site administrator for assistance.</p>
+        <p>Return to the <a href="/transcript">transcript</a>.
 
-      <table>
-        <tr>
-          <th>Course</th>
-          <th>Term</th>
-          <th>Academic Year</th>
-          <th>Grade</th>
-        </tr>
+        <?php else: ?>
 
-        <tr>
-          <td>
-            <?php
-            $courses_result = exec_sql_query(
-              $db,
-              "SELECT number FROM courses WHERE (id = :course_id);",
-              array(
-                ":course_id" => $record["course_id"]
-              )
-            );
-            $course_record = $courses_result->fetchAll()[0];
+        <table>
+          <tr>
+            <th>Course</th>
+            <th>Term</th>
+            <th>Academic Year</th>
+            <th>Grade</th>
+          </tr>
 
-            echo htmlspecialchars($course_record["number"]);
-            ?>
-          </td>
+          <tr>
+            <td>
+              <?php
+              $courses_result = exec_sql_query(
+                $db,
+                "SELECT number FROM courses WHERE (id = :course_id);",
+                array(
+                  ":course_id" => $record["course_id"]
+                )
+              );
+              $course_record = $courses_result->fetchAll()[0];
 
-          <td>
-            <?php echo htmlspecialchars(TERM_CODINGS[$record["term"]]); ?>
-          </td>
+              echo htmlspecialchars($course_record["number"]);
+              ?>
+            </td>
 
-          <td>
-            <?php echo htmlspecialchars(ACADEMIC_YEAR_CODINGS[$record["acad_year"]]); ?>
-          </td>
+            <td>
+              <?php echo htmlspecialchars(TERM_CODINGS[$record["term"]]); ?>
+            </td>
 
-          <td>
-            <?php echo htmlspecialchars($record["grade"]); ?>
-          </td>
-        </tr>
-      </table>
+            <td>
+              <?php echo htmlspecialchars(ACADEMIC_YEAR_CODINGS[$record["acad_year"]]); ?>
+            </td>
 
-      <?php if (isset($_POST["request-insert"])): ?>
+            <td>
+              <?php echo htmlspecialchars($record["grade"]); ?>
+            </td>
+          </tr>
+        </table>
 
-        <h2>Update Confirmation</h2>
-        <p>TODO: update record: <?php echo htmlspecialchars($record_id); ?></p>
+        <?php if (isset($_POST["request-insert"])): ?>
 
-      <?php else: ?>
+          <h2>Update Confirmation</h2>
+          <p>TODO: update record: <?php echo htmlspecialchars($record_id); ?></p>
 
-        <section>
-          <h2>Update Record</h2>
+        <?php else: ?>
 
-          <form class="insert" action="/transcript/update" method="post" novalidate>
+          <section>
+            <h2>Update Record</h2>
 
-            <input type="hidden" name="grade_id" value="<?php echo htmlspecialchars($record["id"]); ?>">
+            <form class="insert" action="/transcript/update" method="post" novalidate>
 
-            <div class="label-input">
-              <label for="insert-grade">Grade:</label>
-              <select id="insert-grade" name="grade">
-                <option value="">No Grade</option>
+              <input type="hidden" name="grade_id" value="<?php echo htmlspecialchars($record["id"]); ?>">
 
-                <?php foreach (GRADES as $grade) { ?>
-                  <option value="<?php echo htmlspecialchars($grade); ?>">
-                    <?php echo htmlspecialchars($grade); ?>
-                  </option>
-                <?php } ?>
-              </select>
-            </div>
+              <div class="label-input">
+                <label for="insert-grade">Grade:</label>
+                <select id="insert-grade" name="grade">
+                  <option value="">No Grade</option>
 
-            <div class="align-right">
-              <button type="submit" name="request-insert">
-                Update Record
-              </button>
-            </div>
-          </form>
-        </section>
+                  <?php foreach (GRADES as $grade) { ?>
+                    <option value="<?php echo htmlspecialchars($grade); ?>">
+                      <?php echo htmlspecialchars($grade); ?>
+                    </option>
+                  <?php } ?>
+                </select>
+              </div>
 
+              <div class="align-right">
+                <button type="submit" name="request-insert">
+                  Update Record
+                </button>
+              </div>
+            </form>
+          </section>
+
+        <?php endif; ?>
       <?php endif; ?>
+
+    <?php else: ?>
+      <p>Please login to view this resource.</p>
+
+      <h3>Sign In</h3>
+
+      <?php echo login_form("/transcript", $session_messages); ?>
     <?php endif; ?>
 
     <p><strong>Note:</strong> No credit is provided for submitting design and/or code that is taken from course-provided examples.Do not copy this design and/or code into your project submission and then change it.</p>
